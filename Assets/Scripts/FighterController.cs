873c7d07de8f5cbf3971af19986bda052e128e81
brian6030 <brianairb1234@gmail.com>
2020-08-17 15:21:16 +0800

    Commit Test

---
+++ Assets/Scripts/FighterController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FighterController : MonoBehaviour
+{
+    public Joystick joystick;
+
+    public float speed;
+
+    [SerializeField] Animator animator;
+    float moveHorizontal, moveVertical;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        moveHorizontal = joystick.Horizontal;
+        moveVertical = joystick.Vertical;
+
+        animator.SetFloat("xAxis", moveHorizontal);
+
+        transform.position += new Vector3(moveHorizontal, 0, moveVertical) * speed * Time.deltaTime;
+
+    }
+}
